a907df396277f1a2adc63070db8487d9a3b37c4f
rickyhkmr09 <43520909+rickyhkmr09@users.noreply.github.com>
2018-10-02 16:58:49 +0700

    Update regristrasi.php

--- regristrasi.php
+++ regristrasi.php
@@ -4,8 +4,64 @@
 	<title>Form Registrasi</title>
 </head>
 <body>
+<form action="koneksi.php" method="POST">
+<table>
+<tr>
+<td>
+NIM
+</td>
+<td>
+:
+</td>
+<td>
+<input type="text" name="nim" required="">
+</td>
+</tr>
 
+<tr>
+<td>
+Nama
+</td>
+<td>
+:
+</td>
+<td>
+<input type="text" name="nama" required="">
+</td>
+</tr>
 
-
+<tr>
+<td>
+Email
+</td>
+<td>
+:
+</td>
+<td>
+<input type="email" name="email" placeholder="@gmail.com" required="">
+</td>
+</tr>
+<tr>
+<td>
+<input type="submit" name="submit" value="input">
+</td>
+</tr>
 </body>
 </html>
+
+<?php
+ if(isset($_POST['submit'])){
+  $nim=$_POST['nim'];
+  $nama=$_POST['nama'];
+  $email=$_POST['email'];
+  
+  $error=array();
+  if(strlen($_POST['nim']) >= 10){
+   $error['nim']=="Harus 10";
+  } if(strlen($_POST['nama']) <= 25){
+   $error['nama']=="Harus 25";
+  } if(empty($email)){
+   $error['email']=="Email harus diisi";
+  }
+ }
+?>
